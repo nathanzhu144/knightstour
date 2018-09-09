8e31555d10de3363196e69a349acfad390706403
nathanzhu144 <znathan@umich.edu>
2018-09-09 13:26:20 -0400

    Testing out shell scrpt for code setup

---
+++ knight_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#making directory
+mkdir knightstour
+cd knightstour
+
+#pulling code from git
+git init
+git pull https://github.com/nathanzhu144/knightstour.git
+
+#makes the code
+make
+./knightstour_main.exe
+
+rm knight_setup.sh
+
